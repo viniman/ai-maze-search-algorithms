c242a4a63c280844ef3a82ff4416472edbf18fc6
igor <junqueira200@gmail.com>
2019-07-10 12:41:12 -0300

    IDASTAR feito

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
 
     //AStar::ASearchAlgorithm(maze);
 
-    IDAStar::IDAStarSearchAlgorithm(maze, Statistics());
+    IDAStar::IDAStarSearchAlgorithm(maze, statistics);
 
     return 0;
 }
